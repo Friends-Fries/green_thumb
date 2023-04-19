cbb6afac6143846cd7cd49cff4b56aaf1d2be03f
gchan26 <guilhermechan30@gmail.com>
2023-04-19 00:51:20 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,26 +25,6 @@ module.exports = {
     },
 
     extend: {
-      animation: {
-        blob: "blob 7s infinite",
-      },
-      keyframes: {
-        blob: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(30px, -50px) scale(1.1)",
-          },
-          "66%": {
-            transform: "translate(-20px, 20px) scale(0.9)",
-          },
-          "100%": {
-            transform: "tranlate(0px, 0px) scale(1)",
-          },
-        },
-      },
-
       screens: {
         sm: "600px",
         md: "960px",
