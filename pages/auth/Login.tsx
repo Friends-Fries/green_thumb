53ac2f287ddb0f55166c03cae1c3a1c1f402210b
gchan26 <guilhermechan30@gmail.com>
2023-08-28 16:56:44 -0300

    Responsiveness for smaller screens

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -9,12 +9,12 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
-import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControlLabel from "@mui/material/FormControlLabel";
 import TextField from "@mui/material/TextField";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 // Images
 import Logo from "../../src/assets/Logo.png";
@@ -43,36 +43,36 @@ function Login() {
   });
 
   const MainBtn = styled(Button)({
-    margin: '15px 0',
-    width: '100%',
-    textTransform: 'none',
+    margin: "15px 0",
+    width: "100%",
+    textTransform: "none",
     fontSize: 20,
-    padding: '12px 80px',
+    padding: "12px 80px",
     lineHeight: 1.5,
-    backgroundColor: '#225123',
-    fontWeight: '600',
-    '&:hover': {
-      backgroundColor: '#1D481E',
-      boxShadow: 'none',
+    backgroundColor: "#225123",
+    fontWeight: "600",
+    "&:hover": {
+      backgroundColor: "#1D481E",
+      boxShadow: "none",
     },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#1D481E',
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#1D481E",
     },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem #409845',
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem #409845",
     },
   });
 
   return (
     <ThemeProvider theme={theme}>
       <body className="bg-gt-main-green-50">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
           <section className="h-screen bg-gradient-to-b from-[#C2F3C3] to-[#8CDF8F] flex justify-center items-center">
-            <div className="bg-white rounded-md w-8/12 p-8">
+            <div className="bg-white rounded-md w-11/12 lg:w-8/12 p-8">
               <div className="flex flex-col items-center">
                 <img src={Logo} className="mx-auto mb-4" alt="Logo" />
-                <Divider variant="middle" className="w-full mb-4"/>
+                <Divider variant="middle" className="w-full mb-4" />
                 <div className="w-full mt-7">
                   <div className="mb-4">
                     <TextField
@@ -120,20 +120,28 @@ function Login() {
                       sx={{ color: "#3B8E3D" }}
                     />
                   </div>
-                  <MainBtn color="secondary" size="large" variant="contained">Entrar</MainBtn>
+                  <MainBtn color="secondary" size="large" variant="contained">
+                    Entrar
+                  </MainBtn>
                   <div className="my-5 text-center font-semibold text-gt-main-green-600 no-underline">
-                        <a><p>Esqueceu a senha?</p></a>
+                    <a>
+                      <p>Esqueceu a senha?</p>
+                    </a>
                   </div>
                 </div>
-                <Divider variant="middle" className="w-full"/>
+                <Divider variant="middle" className="w-full" />
               </div>
               <div className="my-5 text-center text-gt-main-green-600 no-underline">
-                        <a><p>Ainda não possui cadastro? <strong>Criar Conta</strong></p></a>
-                  </div>
+                <a>
+                  <p>
+                    Ainda não possui cadastro? <strong>Criar Conta</strong>
+                  </p>
+                </a>
+              </div>
             </div>
           </section>
-          <section>
-            <img src={PlantBG} className="h-screen w-full" alt="Background" />
+          <section className="hidden lg:block">
+            <img src={PlantBG} className="lg:h-screen lg:w-full" alt="Background" />
           </section>
         </div>
       </body>
