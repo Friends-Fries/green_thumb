d5492eed5f63e6677969286a8b3c21f9122245ae
gchan26 <guilhermechan30@gmail.com>
2023-09-12 12:09:09 -0300

    Created navbar file

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,12 +1,20 @@
 // React
 import React from "react";
 
+// Components
+import Navbar from "../src/components/Navbar";
+
 function Landing() {
 
     return (
+      <>
+      <Navbar />
+      <section className="h-[90vh]">
         <div>
-            Teste
+          
         </div>
+    </section>
+      </>
     )
 }
 
